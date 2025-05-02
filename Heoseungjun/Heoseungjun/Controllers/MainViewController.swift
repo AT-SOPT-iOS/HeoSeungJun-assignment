0d70e99933828b413254d4b219738343a1d86d3b
heoseungjun <gjtmdwns0815@naver.com>
2025-05-02 22:16:00 +0900

    feat: 메인 뷰 컨트롤러 구현

--- Heoseungjun/Heoseungjun/Controllers/MainViewController.swift
+++ Heoseungjun/Heoseungjun/Controllers/MainViewController.swift
@@ -2,7 +2,25 @@ import UIKit
 import SnapKit
 
 class MainViewController: UIViewController {
-
+    
+    private let mainPiecesView = MainPiecesView()
+    private let topPiecesView = TopPiecesView()
+    private let realTimeLivesView = RealTimeLivesView()
+    private let realTimeMoviesView = RealTimeMoviesView()
+    private let baseballTemasView = BaseballTeamsView()
+    private let otherPiecesView = OtherPiecesView()
+    private let masterPiecesView = MasterpiecesView()
+    private let footerView = FooterView()
+    
+    private let scrollView = UIScrollView()
+    
+    private let contentStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        return stackView
+    }()
+    
     private let tvingImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "tvingLogo")
@@ -86,49 +104,105 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        hideBackButton()
+        addViews()
         setLayout()
-        setConstraints()
+    }
+    
+    private func hideBackButton() {
+        self.navigationItem.setHidesBackButton(true, animated: true)
+    }
+    
+    private func addViews() {
+        view.backgroundColor = .black
+        contentStackView.addArrangedSubViews(
+            mainPiecesView,
+            topPiecesView,
+            realTimeLivesView,
+            realTimeMoviesView,
+            baseballTemasView,
+            otherPiecesView,
+            masterPiecesView,
+            footerView
+        )
+        scrollView.addSubview(contentStackView)
+        view.addSubViews(navigationView, menuStackView, scrollView)
     }
     
     private func setLayout() {
-        view.backgroundColor = .black
-        view.addSubview(navigationView)
-        view.addSubview(menuStackView)
-    }
-    
-    private func setConstraints() {
-        navigationView.snp.makeConstraints {
-            $0.top.equalTo(view.snp.top).offset(50)
-            $0.leading.equalTo(view.snp.leading)
-            $0.height.equalTo(71)
-        }
-        
         tvingImageView.snp.makeConstraints {
-            $0.top.equalTo(navigationView.snp.top)
-            $0.leading.equalTo(view.snp.leading)
+            $0.leading.equalToSuperview().offset(20)
+            $0.centerY.equalTo(navigationView.snp.centerY)
             $0.width.equalTo(191)
-            $0.height.equalTo(71)
+            $0.height.equalTo(78)
         }
-        
+
         profileView.snp.makeConstraints {
-            $0.trailing.equalTo(view.snp.trailing).offset(-16)
-            $0.centerY.equalTo(tvingImageView.snp.centerY)
-            $0.width.equalTo(30)
-            $0.height.equalTo(30)
+            $0.trailing.equalToSuperview().inset(20)
+            $0.centerY.equalTo(navigationView.snp.centerY)
+            $0.width.height.equalTo(30)
         }
-        
+
         searchImageView.snp.makeConstraints {
-            $0.trailing.equalTo(profileView.snp.leading).offset(-12)
-            $0.centerY.equalTo(tvingImageView.snp.centerY)
-            $0.width.equalTo(30)
-            $0.height.equalTo(30)
+            $0.trailing.equalTo(profileView.snp.leading).offset(-16)
+            $0.centerY.equalTo(navigationView.snp.centerY)
+            $0.width.height.equalTo(30)
         }
         
+        navigationView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(-40)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(100)
+        }
+
         menuStackView.snp.makeConstraints {
             $0.top.equalTo(navigationView.snp.bottom).offset(10)
-            $0.centerX.equalTo(view.snp.centerX)
+            $0.centerX.equalToSuperview()
             $0.height.equalTo(27)
         }
+
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(menuStackView.snp.bottom).offset(20)
+            $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+
+        contentStackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalTo(scrollView.snp.width)
+        }
+
+        mainPiecesView.snp.makeConstraints {
+            $0.height.equalTo(400)
+        }
+
+        topPiecesView.snp.makeConstraints {
+            $0.height.equalTo(174)
+        }
+
+        realTimeLivesView.snp.makeConstraints {
+            $0.height.equalTo(173)
+        }
+        
+        realTimeMoviesView.snp.makeConstraints {
+            $0.height.equalTo(199)
+        }
+        
+        baseballTemasView.snp.makeConstraints {
+            $0.height.equalTo(80)
+        }
+        
+        otherPiecesView.snp.makeConstraints {
+            $0.height.equalTo(85)
+        }
+        
+        masterPiecesView.snp.makeConstraints {
+            $0.height.equalTo(153)
+        }
+        
+        footerView.snp.makeConstraints {
+            $0.height.equalTo(200)
+            $0.bottom.equalTo(contentStackView.snp.bottom).inset(30)
+        }
     }
-    
+
 }
