ce73d11845e60f06d2d9ded509b80aba01a95390
heoseungjun <gjtmdwns0815@naver.com>
2025-06-03 16:16:24 +0900

    chore: Group 키워드 삭제

--- Heoseungjun/Heoseungjun/Presentation/Views/SwiftUIView/ContentView.swift
+++ Heoseungjun/Heoseungjun/Presentation/Views/SwiftUIView/ContentView.swift
@@ -8,24 +8,23 @@ struct ContentView: View {
             NewHeaderView()
             NewMenuView(selectedIndex: $selectedMenuIndex)
             
-            Group {
-                switch selectedMenuIndex {
-                case 0:
-                    HomeView()
-                case 1:
-                    DramaView()
-                case 2:
-                    EntertainmentView()
-                case 3:
-                    MovieView()
-                case 4:
-                    SportView()
-                case 5:
-                    NewsView()
-                default:
-                    Text("Other")
-                }
+            switch selectedMenuIndex {
+            case 0:
+                HomeView()
+            case 1:
+                DramaView()
+            case 2:
+                EntertainmentView()
+            case 3:
+                MovieView()
+            case 4:
+                SportView()
+            case 5:
+                NewsView()
+            default:
+                Text("Other")
             }
+            
         }
         .background(.black)
     }
